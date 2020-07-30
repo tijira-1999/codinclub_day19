e0004a12127e2e08670a9a8a81cc87cb4d2e839e
tijira-1999 <tijira1999sahoo@gmail.com>
2020-07-30 21:06:02 +0530

    use case 6 completed

---
+++ EmployeeWageUC4.java
@@ -0,0 +1,24 @@
+class EmployeeWageUC4
+{
+	public static void main(String[] args)
+	{
+		final int isfull=1;
+		final int isparttime=2;
+		int emprateperhour=20;
+		int emphours=0;
+		int empwage=0;
+		double empcheck=Math.floor(Math.random() *10)%3;
+		switch((int)empcheck){
+		case isfull:
+			emphours=8;
+			break;
+		case isparttime:
+			emphours=4;
+			break;
+		default:
+			emphours=0;
+		}
+	empwage=emphours*emprateperhour;
+        System.out.println("Employee Wage :"+empwage);
+	}
+}
